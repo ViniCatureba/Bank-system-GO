0831bbe65cc517804fab7ab6dae44739cf788cc0
ViniCatureba <vinicius.bigao14@gmail.com>
2023-02-19 18:25:20 -0300

    new features

--- main.go
+++ main.go
@@ -9,24 +9,49 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string{
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
 	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar{
+	if podeSacar {
 		c.saldo -= valorDoSaque
 		return "Saque realizado com sucesso"
-	}else{
+	} else {
 		return "Saldo insuficiente"
 	}
 }
 
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	} else {
+		return "O valor do deposito menor que 0", c.saldo
+	}
 
+}
+
+func (c *ContaCorrente) Tranferir(valorTransferencia float64, contaDestino *ContaCorrente) bool {
+
+	if valorTransferencia < c.saldo && valorTransferencia > 0 {
+		c.saldo -= valorTransferencia
+		contaDestino.Depositar(valorTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
 
 func main() {
-	contaDaSilvia := ContaCorrente{}
-	contaDaSilvia.titular = "Silvia"
-	contaDaSilvia.saldo = 500
-	fmt.Println(contaDaSilvia.Sacar(300))
+	contaDaSilvia := ContaCorrente{titular: "Silvia", saldo: 500}
+	contaDoGustavo := ContaCorrente{titular: "Gustavo", saldo: 200}
 	fmt.Println(contaDaSilvia.saldo)
+	fmt.Println(contaDoGustavo.saldo)
 
-	
+	status := contaDaSilvia.Tranferir(300, &contaDoGustavo)
+
+	fmt.Println(status)
+
+	fmt.Println(contaDaSilvia.saldo)
+	fmt.Println(contaDoGustavo.saldo)
+	contaDaSilvia.Depositar(500)
+	fmt.Println(contaDaSilvia.saldo)
 }
