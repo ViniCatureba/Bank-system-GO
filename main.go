5618422caa15e417263b61ab3591f660e3a8aed2
ViniCatureba <vinicius.bigao14@gmail.com>
2023-02-22 13:10:04 -0300

    final commit

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"bank/clientes"
 	"bank/contas" // tmb pode dar um apelido como no python "c bank/contas"
 	"fmt"
 )
 
+func PagarBoleto(conta verificarConta, valorBoleto float64) {
+	conta.Sacar(valorBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
-	clienteBruno := contas.ContaCorrente{
-		Titular:       clientes.Titular{},
-		NumeroAgencia: 0,
-		NumeroConta:   0,
-	}
+	clienteBruno := contas.ContaCorrente{}
 	clienteBruno.Depositar(500)
+	PagarBoleto(&clienteBruno, 60)
 	fmt.Println(clienteBruno.ObterSaldo())
 
 }
